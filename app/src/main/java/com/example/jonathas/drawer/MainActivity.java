82203a5592fbcc66ed752136b9ddf670c2afc058
jonathasmedina <emaildojonathas@gmail.com>
2016-10-18 21:21:36 -0300

    -

--- app/src/main/java/com/example/jonathas/drawer/MainActivity.java
+++ app/src/main/java/com/example/jonathas/drawer/MainActivity.java
@@ -109,7 +109,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
